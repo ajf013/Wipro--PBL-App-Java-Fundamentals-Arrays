e0d4ba7de64bfcb15fbe2aa292594a8bcc64ec4a
Francis Cruz <jeni13franc@gmail.com>
2021-07-13 15:05:13 +0530

    Initial commit

---
+++ Assignment6.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Assignment6 {
+
+	public static void main(String[] args) {
+		int[] array = {48, 55, 68, 88, 101, 122};
+		
+		Arrays.sort(array);
+		
+		System.out.println(Arrays.toString(array));
+
+	}
+
+}
